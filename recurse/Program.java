43d8943cfe6206d5a3e29d441c5e6d1a79a3eeec
Weston Vial <wes@wesvial.com>
2015-09-01 13:05:40 -0400

    Make Program better

--- recurse/Program.java
+++ recurse/Program.java
@@ -1,16 +1,47 @@
-import java.io.*;
+//import java.lang.Math.*;
+//import java.security.*;
+import java.lang.instrument.Instrumentation;
 import java.util.*;
-//import java.lang.instrument.Instrumentation;
+import java.io.*;
 
 public class Program {
     public static Rainbow table = new Rainbow();
 
     public static void main(String[] args) {
-        System.out.println("\n**** TABLES GENERATED ****");
-        table.writeToFile("RAINBOW_TABLE.ser");
         int success = 0;
         int rejects = 0;
         long time1, time2;
+
+        System.out.println("\n*********** T A B L E S  G E N E R A T E D ***********");
+        table.writeToFile("RAINBOW_TABLE.ser");
+
+        System.out.println("\n*********** F I L E ***********");
+        fromFile();
+        System.out.println("\n*******************************");
+
+        System.out.println("\n*********** T E S T S ***********");
+        byte[][] words = new byte[1000][3];
+        for (int i = words.length; i < (2 * words.length); i++) {
+            words[i] = table.intToBytes()
+        }
+
+        System.out.println("\n****  Invert - 1000 row table");
+        time1 = System.currentTimeMillis();
+        for (int i = 0; i < words.length; i++) {
+            digest = table.hash(words[i]);
+            result = table.invert(digest);
+            if (result != null) {
+                success++;
+            } else {
+                rejects++;
+            }
+        }
+        time2 = System.currentTimeMillis();
+        System.out.println("Total time: " + ((time2-time1)/1000.0) + " seconds.");
+        System.out.println("Success rate: " + success + " / 1000 (");
+        System.out.println("Rejection rate: " + reject + " / 1000 (");
+
+
     }
 
     /*public static void fromFile() {
